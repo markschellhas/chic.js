396763811c841a05defe7d01e0f203bb70b28b78
Mark <mark@schellhas.se>
2023-07-29 17:07:39 +0200

    adds to test coverage, changes edit to update

---
+++ test/page_templates.test.js
@@ -0,0 +1,63 @@
+import {
+  linksOnIndexPageTemplate,
+  mainContentOnShowPage,
+  linksOnShowPageTemplate,
+  mainContentOnIndexPage,
+} from "../lib/templates/page_templates.js";
+
+describe("page_templates", () => {
+  describe("linksOnIndexPageTemplate", () => {
+    it("returns the expected HTML", () => {
+      const resourceName = "posts";
+      const expected = `
+  <div class="links">
+    <a href="/${resourceName}/new">Create a new post</a>
+  </div>
+`;
+      expect(linksOnIndexPageTemplate(resourceName)).toEqual(expected);
+    });
+  });
+
+  describe("mainContentOnShowPage", () => {
+    it("returns the expected HTML", () => {
+      const resourceName = "posts";
+      const expected = `
+    {#each Object.entries(data.post) as [key, val]}
+      <p><b>{key}:</b> {val}</p>
+    {/each}
+`;
+      expect(mainContentOnShowPage(resourceName)).toEqual(expected);
+    });
+  });
+
+  describe("linksOnShowPageTemplate", () => {
+    it("returns the expected HTML", () => {
+      const resourceName = "posts";
+      const expected = `
+  <div class="links">
+    <a href="/${resourceName}">Back to posts</a> <a href="/${resourceName}/{data.post.id}/edit">Edit post</a>
+</div>
+`;
+      expect(linksOnShowPageTemplate(resourceName)).toEqual(expected);
+    });
+  });
+
+  describe("mainContentOnIndexPage", () => {
+    it("returns the expected HTML", () => {
+      const resourceName = "posts";
+      const firstFieldName = "title";
+      const expected = `
+    <ul>
+    {#each data.posts as item}
+      <li>
+          <a href={"/posts/"+item.id}>{item.title}</a>
+      </li>
+    {/each}
+    </ul>
+`;
+      expect(mainContentOnIndexPage(resourceName, firstFieldName)).toEqual(
+        expected
+      );
+    });
+  });
+});
--- test/templates.test.js
+++ test/templates.test.js
@@ -1,7 +1,8 @@
-import { routeServerIndexPageTemplate } from "../lib/templates/page_templates";
+import { routeServerIndexPageTemplate, routeServerShowPageTemplate, routeServerEditPageTemplate,  } from "../lib/templates/page_templates";
 
 describe('route server index page template', () => {
     it('return the template in the correct format', () => {
+
       expect(routeServerIndexPageTemplate('cats')).toEqual(`
 import { listCats, createCat, destroyCat } from '$lib/controllers/cats/cats_controller';
 import { fail } from '@sveltejs/kit';
@@ -10,7 +11,8 @@ import { fail } from '@sveltejs/kit';
 export const actions = {
     new: async ({ request }) => {
         const data = await request.formData();
-        return createCat(data)
+        const body = Object.fromEntries(data.entries());
+        return createCat(body)
             .then(cat => {
                 console.log('cat created: ', cat);
                 return { success: true };
@@ -22,7 +24,8 @@ export const actions = {
     },
     destroy: async ({ request }) => {
         const data = await request.formData();
-        return destroyCat(data)
+        const body = Object.fromEntries(data.entries());
+        return destroyCat(body)
             .then(cat => {
                 console.log('cat destroyed: ', cat);
                 return { success: true };
@@ -44,5 +47,60 @@ export async function load({ params }) {
 }
 `);
     });
-  
-  });
+
+});
+
+describe('route server show page template', () => {
+    it('return the template in the correct format', () => {
+
+      expect(routeServerShowPageTemplate('cats')).toEqual(`
+import { getCat, updateCat } from '$lib/controllers/cats/cats_controller';
+import { fail } from '@sveltejs/kit';
+
+export async function load({ params }) {
+    const { id } = params;
+    const x = await getCat(parseInt(id));
+    const cat = x.get();
+    return {
+        cat: cat
+    };
+}
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    update: async ({ request }) => {
+        const data = await request.formData();
+        const body = Object.fromEntries(data.entries());
+        return updateCat(body)
+            .then(cat => {
+                console.log('cat updated: ', cat);
+                return { success: true };
+            })
+            .catch(err => {
+                console.log('cat failed to update');
+                return fail(err);
+            });
+    }
+};
+`);
+    })
+});
+
+describe('route server edit page template', () => {
+    it('return the template in the correct format', () => {
+
+      expect(routeServerEditPageTemplate('colors')).toEqual(`
+import { getColor } from '$lib/controllers/colors/colors_controller';
+
+// Get the color to edit
+export async function load({ params }) {
+    const { id } = params;
+    const x = await getColor(parseInt(id));
+    const color = x.get();
+    return {
+        color: color
+    };
+}
+`);
+    })
+});
